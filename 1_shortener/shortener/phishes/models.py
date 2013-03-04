19ec0eb39843c643c235288706056b177a77a5fb
Riley Strong <strongriley@gmail.com>
2013-03-03 18:21:45 -0800

    initial phish models

--- 1_shortener/shortener/phishes/models.py
+++ 1_shortener/shortener/phishes/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+
+class PhishUrl(models.Model):
+    """
+    Verified malicious URL from phishtank.com
+    http://www.phishtank.com/developer_info.php
+    """
+    phish_id = models.IntegerField()
+    url = models.CharField(max_length=2048)
+    inserted = models.DateTimeField(auto_now_add=True)
+
+
+class PhishDomain(models.Model):
+    """
+    For simplified blacklisting, a domain that has at least 1
+    verified phishing URL under it.
+
+    Any shortenings matching a domain will not be allowed.
+    """
+    domain_name = models.CharField(max_length=2048, unique=True)
+    inserted = models.DateTimeField(auto_now_add=True)
+

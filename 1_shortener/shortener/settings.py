9f604b3a5b8ddd7ef52669b11a2c896814964366
Riley Strong <strongriley@gmail.com>
2013-03-04 22:31:09 -0800

    comment out WSGI

--- 1_shortener/shortener/settings.py
+++ 1_shortener/shortener/settings.py
@@ -106,7 +106,7 @@ MIDDLEWARE_CLASSES = (
 ROOT_URLCONF = 'shortener.urls'
 
 # Python dotted path to the WSGI application used by Django's runserver.
-WSGI_APPLICATION = 'shortener.wsgi.application'
+#WSGI_APPLICATION = 'shortener.wsgi.application'
 
 TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".

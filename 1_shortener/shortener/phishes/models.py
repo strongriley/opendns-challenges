334ab2d6aafbeb53cf615aa7c26f97af52a6ea18
Riley Strong <strongriley@gmail.com>
2013-03-06 22:59:01 -0800

    Cleanup phishes app
    - Switch to cleaner URLField.
    - Remove placeholder files.

--- 1_shortener/shortener/phishes/models.py
+++ 1_shortener/shortener/phishes/models.py
@@ -7,7 +7,7 @@ class PhishUrl(models.Model):
     http://www.phishtank.com/developer_info.php
     """
     phish_id = models.IntegerField(unique=True)
-    url = models.CharField(max_length=2048, unique=True)
+    url = models.URLField(max_length=2048, unique=True)
     inserted = models.DateTimeField(auto_now_add=True)
 
     class Meta:

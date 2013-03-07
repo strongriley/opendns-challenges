8f23517cecde16f6b1730de5f4e594b97dcae259
Riley Strong <strongriley@gmail.com>
2013-03-06 22:59:10 -0800

    Shortenings unit tests on the model

--- 1_shortener/shortener/shortenings/tests.py
+++ 1_shortener/shortener/shortenings/tests.py
@@ -1,16 +1,45 @@
-"""
-This file demonstrates writing tests using the unittest module. These will pass
-when you run "manage.py test".
-
-Replace this with more appropriate tests for your application.
-"""
-
 from django.test import TestCase
 
+from shortener.shortenings.models import Shortening
+from shortener.phishes.models import PhishUrl
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
+
+class ShorteningTests(TestCase):
+    """
+    Test behavior associated with the Shortening Model.
+    """
+    def test_random_short_id(self):
         """
-        Tests that 1 + 1 always equals 2.
+        Create few hundred URLs, none should be the same.
+        More a spot test. The uniqueness on short_id will ensure all's good.
         """
-        self.assertEqual(1 + 1, 2)
+        url = "http://example.com"
+        n = 500
+        for i in range(n):
+            obj = Shortening(url=url)
+            obj.full_clean()
+            obj.save()
+        self.assertEqual(Shortening.objects.filter(url=url).count(), n)
+
+    def test_check_is_safe_true(self):
+        PhishUrl.objects.create(phish_id=1, url="http://is-safe-true.com")
+        short = Shortening(url="http://opendns.com")
+        short.full_clean()
+        short.save()
+        self.assertTrue(short.check_is_safe())
+
+    def test_check_is_safe_false(self):
+        PhishUrl.objects.create(phish_id=1, url="http://facebook.com")
+        short = Shortening(url="http://facebook.com")
+        short.full_clean()
+        short.save()
+        self.assertFalse(short.check_is_safe())
+
+    def test_url_visited(self):
+        short = Shortening(url="http://rileystrong.com")
+        short.full_clean()
+        short.save()
+        self.assertEqual(short.visits, 0)
+        for i in range(10):
+            short.url_visited()
+        self.assertEqual(short.visits, 10)

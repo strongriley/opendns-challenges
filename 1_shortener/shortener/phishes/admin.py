18165d8f675103fbe64f36795d4160a366b5fbf5
Riley Strong <strongriley@gmail.com>
2013-03-06 17:19:56 -0800

    Finish removing PhishDomain model

--- 1_shortener/shortener/phishes/admin.py
+++ 1_shortener/shortener/phishes/admin.py
@@ -1,7 +1,5 @@
 from django.contrib import admin
 
 from shortener.phishes.models import PhishUrl
-from shortener.phishes.models import PhishDomain
 
 admin.site.register(PhishUrl)
-admin.site.register(PhishDomain)

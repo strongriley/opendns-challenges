b51e0b41d475813b48067dcf5ca90fc6cb7b0aaa
Riley Strong <strongriley@gmail.com>
2013-03-06 18:06:32 -0800

    Detail view with placeholder template

--- 1_shortener/shortener/shortenings/views.py
+++ 1_shortener/shortener/shortenings/views.py
@@ -1,10 +1,14 @@
 from django.views.generic import CreateView
+from django.views.generic import DetailView
+from django.views.generic import RedirectView
+from django.shortcuts import get_object_or_404
 
+from shortener.shortenings.forms import ShorteningForm
 from shortener.shortenings.models import Shortening
 
 
-class ShorteningCreate(CreateView):
-    model = Shortening
+class ShorteningCreateView(CreateView):
+    form_class = ShorteningForm
     template_name = 'index.html'
 
 
@@ -15,3 +19,10 @@ class ProtectedRedirectView(RedirectView):
     def get_redirect_url(self, short_id):
         shortening = get_object_or_404(Shortening, short_id=short_id)
         raise TypeError("not ready!")
+
+
+class ShorteningDetailView(DetailView):
+    model = Shortening
+    template_name = 'detail.html'
+    slug_field = 'short_id'
+    slug_url_kwarg = 'short_id'
